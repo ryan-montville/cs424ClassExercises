aa7893277306209982f6ef1da63170a5a3958104
Ryan Montville <rmontville@luc.edu>
2025-11-02 10:19:56 -0600

    fixing search and adding tags

--- week_9-current/app.js
+++ week_9-current/app.js
@@ -1,6 +1,7 @@
 let booksList = [];
 let booksToDisplay = [];
 let years = [];
+let messageString = "";
 
 let booksContainer = document.getElementById('books-container');
 let searchForm = document.getElementById('search');
@@ -50,9 +51,14 @@ function searchBooks() {
         createMessage("Please do not leave to search field empty.", "message", "error");
     } else {
         booksToDisplay = booksList.filter(book => {
-            return book['bookTitle'].toLowerCase().includes(searchTerm.toString().trim().toLowerCase());
+            let authorString = book['authors'].join(', ');
+            return book['bookTitle'].toLowerCase().includes(searchTerm.toString().trim().toLowerCase()) || authorString.toLowerCase().includes(searchTerm.toString().trim().toLowerCase());
         });
-        console.log(booksToDisplay.length);
+        if (booksToDisplay.length > 0) {
+            messageString = `${booksToDisplay.length} books matching "${searchTerm.toString().trim()}"`;
+        } else {
+            messageString = `No results for "${searchTerm.toString().trim()}"`;
+        }
         displayBooks();
     }
 }
@@ -81,7 +87,6 @@ function loadYears(bokList) {
         yearButton.setAttribute('type', 'button');
         yearButton.addEventListener('click', () => {
             filterForYear(year);
-
         });
         filerYearsCard.appendChild(yearButton);
     });
@@ -91,12 +96,14 @@ function loadYears(bokList) {
 function filterForYear(year) {
     if (year === 'all') {
         booksToDisplay = booksList;
+        messageString = `Displaying ${booksList.length} books`;
     } else {
         const yearToFilter = new Date(year, 0, 1).getFullYear();
         booksToDisplay = booksList.filter(book => {
             const bookYear = new Date(book['dateRead']).getFullYear();
             return bookYear === yearToFilter;
         });
+        messageString = `Displaying ${booksToDisplay.length} for ${year}`;
     }
     displayBooks();
 }
@@ -114,34 +121,55 @@ async function loadData() {
         booksToDisplay = data;
         displayBooks();
         loadYears(booksList);
+        messageString = `Displaying ${booksList.length} books`;
     } catch (error) {
         createMessage(error, 'message', 'error');
     }
 }
 
 function displayBooks() {
+    //Set the title for how many books are displayed
+    let resultTitle = document.getElementById('result-message');
+    resultTitle.innerHTML = '';
+    let resultTitleText = document.createTextNode(messageString);
+    resultTitle.appendChild(resultTitleText);
     booksContainer.innerHTML = '';
+    //Add the books to the books container
     booksToDisplay.forEach(book => {
+        //Create a book article
         let bookCard = document.createElement('article');
         bookCard.setAttribute('class', 'book card');
+        //Add the cover art
         let bookCover = document.createElement('img');
         bookCover.setAttribute('src', book['cover']);
         bookCover.setAttribute('alt', book['bookTitle']);
         bookCard.appendChild(bookCover);
         let infoSection = document.createElement('section');
+        //Add the book title
         let bookTitleH2 = document.createElement('h2');
         let bookTitle = document.createTextNode(book['bookTitle']);
         bookTitleH2.appendChild(bookTitle);
         infoSection.appendChild(bookTitleH2);
+        //Add the authors
         let authorH3 = document.createElement('h3');
         let authorName = document.createTextNode(`By: ${book['authors'].join(' ')}`);
         authorH3.appendChild(authorName);
         infoSection.appendChild(authorH3);
+        //Add the date read
         const date = new Date(book['dateRead']);
         let readDateP = document.createElement('p');
         let readDate = document.createTextNode(`Date Read: ${date.toLocaleString('default', { month: 'long' })} ${date.getFullYear()}`);
         readDateP.appendChild(readDate);
         infoSection.appendChild(readDateP);
+        //Add the tags for the book
+        const tagsList = document.createElement('ul');
+        book['tags'].forEach(tag => {
+            let tagLi = document.createElement('li');
+            let tagName = document.createTextNode(tag);
+            tagLi.appendChild(tagName);
+            tagsList.appendChild(tagLi);
+        });
+        infoSection.appendChild(tagsList);
         bookCard.appendChild(infoSection);
         booksContainer.appendChild(bookCard);
     });
@@ -153,4 +181,5 @@ loadData();
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     searchBooks();
+    searchForm.reset();
 })
