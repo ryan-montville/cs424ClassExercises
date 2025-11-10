289c0ee3e1bcac3f3232c307bc56a72badac5922
Ryan Montville <rmontville@luc.edu>
2025-11-10 14:58:02 -0500

    finished part 4

--- week-11/app.js
+++ week-11/app.js
@@ -18,25 +18,32 @@ const loadButtonPart3 = document.getElementById('loadP3');
 const loadButtonPart4Good = document.getElementById('part4Good');
 const loadButtonPart4Bad = document.getElementById('part4Bad');
 
-
-const loadCityPart3 = file => {
-    //Display loading message
+function displayLoadingMessage() {
     outputSection.innerHTML = '';
     const loadingP = document.createElement('p');
     const loadingText = document.createTextNode("Loading...");
     loadingP.appendChild(loadingText);
     outputSection.appendChild(loadingP);
-    //Fetch the json file
-    const data = fetch(file);
-    //Load the data
-    data.then(response => response.json())
-        .then(jsonData => {
-            console.log(jsonData);
-            //Create the city DOM elements
+}
+
+function displayErrorMessage(errorMessage) {
+    outputSection.innerHTML = '';
+    const errorH2 = document.createElement('h2');
+    const errorH2Text = document.createTextNode("Error loading data");
+    errorH2.appendChild(errorH2Text);
+    outputSection.appendChild(errorH2);
+    const errorMessageP = document.createElement('p');
+    const errorMessageText = document.createTextNode(errorMessage);
+    errorMessageP.appendChild(errorMessageText);
+    outputSection.appendChild(errorMessageP);
+}
+
+function displayCityInfo(cityObject) {
+//Create the city DOM elements
             const city = document.createElement('article');
             //City name
             const cityNameH2 = document.createElement('h2');
-            const cityName = document.createTextNode(jsonData['cityName']);
+            const cityName = document.createTextNode(cityObject['cityName']);
             cityNameH2.appendChild(cityName);
             city.appendChild(cityNameH2);
             //Attractions
@@ -44,7 +51,7 @@ const loadCityPart3 = file => {
             const attractionsH3Text = document.createTextNode("Attractions");
             attractionsH3.appendChild(attractionsH3Text);
             city.appendChild(attractionsH3);
-            const attractionsUl = jsonData['attractions'].reduce((accumulator, currentVal) => {
+            const attractionsUl = cityObject['attractions'].reduce((accumulator, currentVal) => {
                 const attractionLi = document.createElement('li');
                 const attractionName = document.createTextNode(currentVal);
                 attractionLi.appendChild(attractionName);
@@ -57,7 +64,7 @@ const loadCityPart3 = file => {
             const weatherH3Text = document.createTextNode("Weather");
             weatherH3.appendChild(weatherH3Text);
             city.appendChild(weatherH3);
-            const weatherUl = jsonData['weather'].reduce((accumulator, currentVal) => {
+            const weatherUl = cityObject['weather'].reduce((accumulator, currentVal) => {
                 const weatherLi = document.createElement('li');
                 const weather = document.createTextNode(`${currentVal['season']}: ${currentVal['typicalCondition']}`);
                 weatherLi.appendChild(weather);
@@ -67,11 +74,11 @@ const loadCityPart3 = file => {
             city.appendChild(weatherUl);
             //Visited
             const visitedP = document.createElement('p');
-            if (jsonData['visited']) {
-                const visited = document.createTextNode(`I have visited ${jsonData['cityName']}`);
+            if (cityObject['visited']) {
+                const visited = document.createTextNode(`I have visited ${cityObject['cityName']}`);
                 visitedP.appendChild(visited);
             } else {
-                const visited = document.createTextNode(`I have not visited ${jsonData['cityName']} yet`);
+                const visited = document.createTextNode(`I have not visited ${cityObject['cityName']} yet`);
                 visitedP.appendChild(visited);
             }
             city.appendChild(visitedP);
@@ -79,12 +86,54 @@ const loadCityPart3 = file => {
             outputSection.innerHTML = '';
             //Add the city to output
             outputSection.appendChild(city);
+
+}
+const loadCityPart3 = file => {
+    //Display loading message
+    displayLoadingMessage();
+    //Fetch the json file
+    const data = fetch(file);
+    //Load the data
+    data.then(response => response.json())
+        .then(jsonData => {
+            console.log(jsonData);
+            //Display the data in the dom
+            displayCityInfo(jsonData);
         });
 }
 
 //----------------Exercise JSON Part 4----------------
-function loadCityPart4() {
+function loadCityPart4(file) {
+    displayLoadingMessage();
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
 
+        //Checking for browser CORS support
+        if (!('withCredentials' in xhr)) {
+			alert('no CORS support');
+			return;
+		}
+
+        xhr.open("GET", file);
+
+        xhr.onload = function() {
+            try {
+                if (this.status === 200) {
+                    resolve(displayCityInfo(JSON.parse(this.response)));
+                } else {
+                    reject(displayErrorMessage(`${this.status} ${this.statusText}`));
+                }
+            } catch (error) {
+                reject(error.message);
+            }
+        };
+
+        xhr.onerror = function() {
+            reject(displayErrorMessage(`${this.status} ${this.statusText}`));
+        };
+
+        xhr.send();
+    })
 }
 
 loadButtonPart3.addEventListener( 'click', () => (loadCityPart3('city.json')) );
